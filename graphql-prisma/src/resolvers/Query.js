17adb13ce1b6dd1fdd1f873cd1dfcf6e8e91383d
Khanh Luu <luukhanh96@gmail.com>
2020-11-02 23:29:52 +0100

    authentication - lock down queries

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,3 +1,5 @@
+import { getUserId } from '../utils/getUserId';
+
 export const Query = {
   users(parent, args, { prisma }, info) {
     const opArgs = {};
@@ -19,19 +21,46 @@ export const Query = {
   },
 
   posts(parent, args, { prisma }, info) {
-    const opArgs = {};
+    const opArgs = {
+      where: {
+        published: true,
+      },
+    };
 
     if (args.query) {
-      opArgs.where = {
-        OR: [
-          {
-            title_contains: args.query,
-          },
-          {
-            body_contains: args.query,
-          },
-        ],
-      };
+      opArgs.where.OR = [
+        {
+          title_contains: args.query,
+        },
+        {
+          body_contains: args.query,
+        },
+      ];
+    }
+
+    return prisma.query.posts(opArgs, info);
+  },
+
+  myPosts(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    const opArgs = {
+      where: {
+        author: {
+          id: userId,
+        },
+      },
+    };
+
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query,
+        },
+        {
+          body_contains: args.query,
+        },
+      ];
     }
 
     return prisma.query.posts(opArgs, info);
@@ -39,5 +68,40 @@ export const Query = {
 
   comments(parent, args, { prisma }, info) {
     return prisma.query.comments(null, info);
+  },
+
+  async post(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request, false);
+
+    const posts = await prisma.query.posts(
+      {
+        where: {
+          id: args.id,
+          OR: [
+            {
+              author: {
+                id: userId,
+              },
+            },
+            {
+              published: true,
+            },
+          ],
+        },
+      },
+      info
+    );
+
+    if (posts.length === 0) {
+      throw new Error('No post found');
+    }
+
+    return posts[0];
+  },
+
+  me(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    return prisma.query.user({ where: { id: userId } }, info);
   },
 };
--- graphql-prisma/src/utils/getUserId.js
+++ graphql-prisma/src/utils/getUserId.js
@@ -1,14 +1,18 @@
 import jwt from 'jsonwebtoken';
 
-export const getUserId = (request) => {
+export const getUserId = (request, requireAuth = true) => {
   const header = request.request.headers.authorization;
 
-  if (!header) {
+  if (header) {
+    const token = header.replace('Bearer ', '');
+    const decoded = jwt.verify(token, 'thisisasecret');
+
+    return decoded.userId;
+  }
+
+  if (requireAuth) {
     throw new Error('Authentication required');
   }
 
-  const token = header.replace('Bearer ', '');
-  const decoded = jwt.verify(token, 'thisisasecret');
-
-  return decoded.userId;
+  return null;
 };
